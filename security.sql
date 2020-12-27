b936c1e2ab4b76116366ef4b7568c143178f0e59
Konstantinos <kwstas654321@gmail.com>
2020-12-27 12:38:46 +0200

    Comment out tables

--- security.sql
+++ security.sql
@@ -3,27 +3,31 @@ CREATE DATABASE IF NOT EXISTS `security`;
 
 USE `security`;
 
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `active` int(11) DEFAULT NULL,
-  `first_name` varchar(255) NOT NULL,
-  `last_name` varchar(255) NOT NULL,  
-  `email` varchar(255) NOT NULL,  
-  `password` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-);
+-- We create only the database and the tables are created automatically
 
-CREATE TABLE `roles` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `role` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-);
 
-CREATE TABLE `users_roles` (
-  `user_id` int(11) NOT NULL,
-  `role_id` int(11) NOT NULL,
-  PRIMARY KEY (`user_id`,`role_id`),
-  KEY `FKa68196081fvovjhkek5m97n3y` (`role_id`),
-  CONSTRAINT `FK859n2jvi8ivhui0rl0esws6o` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
-  CONSTRAINT `FKa68196081fvovjhkek5m97n3y` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`)
-)
+-----------------------------------------------------
+-- CREATE TABLE `users` (
+--   `id` int(11) NOT NULL AUTO_INCREMENT,
+--   `active` int(11) DEFAULT NULL,
+--   `first_name` varchar(255) NOT NULL,
+--   `last_name` varchar(255) NOT NULL,  
+--   `email` varchar(255) NOT NULL,  
+--   `password` varchar(255) NOT NULL,
+--   PRIMARY KEY (`id`)
+-- );
+
+-- CREATE TABLE `roles` (
+--   `id` int(11) NOT NULL AUTO_INCREMENT,
+--   `role` varchar(255) DEFAULT NULL,
+--   PRIMARY KEY (`id`)
+-- );
+
+-- CREATE TABLE `users_roles` (
+--   `user_id` int(11) NOT NULL,
+--   `role_id` int(11) NOT NULL,
+--   PRIMARY KEY (`user_id`,`role_id`),
+--   KEY `FKa68196081fvovjhkek5m97n3y` (`role_id`),
+--   CONSTRAINT `FK859n2jvi8ivhui0rl0esws6o` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+--   CONSTRAINT `FKa68196081fvovjhkek5m97n3y` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`)
+-- )
