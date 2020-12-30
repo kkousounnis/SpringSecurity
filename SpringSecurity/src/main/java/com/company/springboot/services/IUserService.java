0827ce93a58684d90a52043735979f90d142952d
Konstantinos <kwstas654321@gmail.com>
2020-12-30 12:56:52 +0200

    Registration with validations

--- SpringSecurity/src/main/java/com/company/springboot/services/IUserService.java
+++
@@ -1,9 +0,0 @@
-package com.company.springboot.services;
-
-import com.company.springboot.entities.User;
-import com.company.springboot.entities.dto.UserRegistrationDto;
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface IUserService extends UserDetailsService{
-    public  User save(UserRegistrationDto registrationDto);
-}
